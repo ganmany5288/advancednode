10e4bf19a614b009c5ee2899732a531279091177
James Tang <52274101+ganmany5288@users.noreply.github.com>
2024-05-13 23:16:34 -0700

    test

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 app.route('/').get((req, res) => {
-
+  // Finally, use res.render() in the route for your home page, passing index as the first argument. This will render the pug template.
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;

9e8628c02e2e734ea22051dd9de1084f0f04fd31
James Tang <52274101+ganmany5288@users.noreply.github.com>
2024-05-29 14:56:29 -0700

    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
+const session = require('express-session');
+const passport = require('passport');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
+const {ObjectID} = require('mongodb');
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -12,10 +14,53 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-app.route('/').get((req, res) => {
-  // Finally, use res.render() in the route for your home page, passing index as the first argument. This will render the pug template.
-  res.render('index');
+
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {secure: false}
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+myDB(async client  =>{
+  const myDataBase = await client.db('database').collection('users');
+  app.route('/').get((req, res) => {
+    // Finally, use res.render() in the route for your home page, passing index as the first argument. This will render the pug template.
+    console.log("connected to database!!!");
+    res.render('index', 
+    {
+      title: 'Nihao', 
+      message: 'Please login senpai!'
+    });
+  });
+
+
+  passport.serializeUser((user, done) =>{
+    done(null, user._id);
+  
+  })
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({_id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    })
+  })
+
+
+
+}).catch(err => {
+  app.route('/').get((req, res) => {
+    console.log('NOT connected to DB');
+    res.render('index', {title: err, message: "Unable to connect to database"});
+  });
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
